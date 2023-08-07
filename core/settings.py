43869df5a975bf122a22e88a90d8a8fc94bc663c
Frederick Ngoiya <17891031+FrederickNgoiya@users.noreply.github.com>
2023-08-07 11:03:43 +0300

    added web service url

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ if not SECRET_KEY:
 DEBUG = "RENDER" not in os.environ
 
 # HOSTs List
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "appseed.azurewebsites.net"]
 
 # Add here your deployment HOSTS
 CSRF_TRUSTED_ORIGINS = [
